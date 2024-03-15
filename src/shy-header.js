127f9137c966f29b65895e8a53fa277b1a16a28e
Tim Farnam <twfarnam@gmail.com>
2024-03-15 17:51:52 -0600

    adds shy-header element

---
+++ src/shy-header.js
@@ -0,0 +1,41 @@
+
+document.head.insertAdjacentHTML('beforeend', `
+<style>
+  shy-header {
+    display: block;
+    position: sticky;
+    top: 0;
+    transition: transform 0.3s ease-out;
+  }
+
+  shy-header.hidden {
+    transform: translateY(-100%);
+  }
+</style>
+`)
+
+// better name
+class ShyHeader extends HTMLElement {
+  #lastScroll = 0
+  #lastMaxScroll = 0
+
+  constructor() {
+    super()
+    window.addEventListener('scroll', this.#onScroll)
+  }
+
+  #onScroll = () => {
+    if (window.scrollY > this.#lastScroll) {
+      this.classList.add('hidden')
+    } else if (window.scrollY < this.#lastMaxScroll - 100) {
+      this.#lastMaxScroll = window.scrollY
+      this.classList.remove('hidden')
+    }
+    this.#lastScroll = window.scrollY
+    if (window.scrollY > this.#lastMaxScroll) {
+      this.#lastMaxScroll = window.scrollY
+    }
+  }
+}
+
+customElements.define('shy-header', ShyHeader)
